c04f9731a44585f3e407bd38f305548f64eb7de1
Luis <luiswoohoo@gmail.com>
2022-10-09 06:34:58 -0500

    Moved serverless functions

---
+++ functions/get-movie.js
@@ -0,0 +1,27 @@
+const process = require('process')
+
+const axios = require('axios')
+
+const handler = async function (event) {
+  const movieId = event.queryStringParameters.movieId
+
+  const { API_SECRET, API_URL } = process.env
+  const URL = `${API_URL}?i=${movieId}&apikey=${API_SECRET}`
+
+  try {
+    const { data } = await axios.get(URL)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+  } catch (error) {
+    const { data, headers, status, statusText } = error.response
+    return {
+      statusCode: error.response.status,
+      body: JSON.stringify({ status, statusText, headers, data }),
+    }
+  }
+}
+
+module.exports = { handler }
